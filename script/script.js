ffa55e5d025c265297d20bff953d792abd750c56
Ricky Thakar <rickythakar@gmail.com>
2022-04-06 23:39:35 -0400

    created an array for questions and began writing function for starting the quiz. Also linked the startquiz button to the document

--- script/script.js
+++ script/script.js
@@ -1,9 +1,39 @@
+console.log ("javascript file is hooked up");
+
 // Declare and set variables for the sheet
-var quiz
 var timer
 var answer
-
+var button 
+var quizQuestion
 
 
 // Create an array of possible choices and answers
 
+array [{
+    question: "question 1",
+    choices: ["answer1" , "answer2" , "answer3" , "answer4"],
+    answer: "answer2"
+},
+{
+    question: "question 1",
+    choices: ["answer1" , "answer2" , "answer3" , "answer4"],
+    answer: "answer2"
+}
+
+];
+
+// multiple choice quiz should have 4 variables for each question
+// 1 variable will be true - the false variable will remove time from the timer
+
+// Create function that connects to button
+function startquiz (event){ 
+
+}
+// draft of function
+// startBtn.answer function {
+//     start.timer 
+
+// }
+
+// button should link to array and trigger timer
+document.querySelector(".start").addEventListener("click",startquiz)
