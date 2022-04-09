111e03bf6b9dba3266ed8fc147bcec5a70504f02
Ricky Thakar <rickythakar@gmail.com>
2022-04-09 12:01:45 -0400

    Modified js file and tested it on the website. Did some minor formatting to the webpage to clean the code

--- script/script.js
+++ script/script.js
@@ -1,14 +1,11 @@
 console.log ("javascript file is hooked up");
-
 // Declare and set variables for the sheet
 let timer = 5;
 let countdown;
 let thisQuestion = 0;
 // let quizQuestion //array
 
-
 // Create an array of possible choices and answers
-
 let quizQuestions = [{
     question: "question 1",
     choices: ["answer1" , "answer2" , "answer3" , "answer4"],
@@ -40,7 +37,9 @@ let quizQuestions = [{
 // multiple choice quiz should have 4 variables for each question
 // 1 variable will be true - the false variable will remove time from the timer
 
-// Create function that connects to button
+
+
+// Create function that connects to button to start the quiz
 function startquiz (event){ 
     //This is the function to start the timer of the game
     countdown = setInterval(function () {
@@ -56,14 +55,34 @@ function startquiz (event){
     }, 1000);
     //Hides start button after starting quiz
     document.querySelector(".quiz").style.display = "none";
-    // show first question on the page
+
+
+
+
+    // function to show first question on the page
+
+
+    //cycle through questions on the page 
+
+
+
+
+
+
+
+
+
+
+
+
     document.querySelector(".question").textContent = quizQuestions[thisQuestion].question;
     document.querySelector(".answers").textContent = quizQuestions[thisQuestion].answers;
     //loop over the answers 
+
+
     //quizQuestions[thisQuestion].answers.length
+
     //append each one to the answers div
-
-
 
 
 
@@ -82,6 +101,10 @@ document.querySelector(".start").addEventListener("click",startquiz)
 
 
 
+
+
+
+
 //question 1
 //Inside which HTML element do we put the JavaScript?
 //<javascript>
@@ -92,14 +115,11 @@ document.querySelector(".start").addEventListener("click",startquiz)
 //question 2
 //
 
-
 //queastion 3
 //
 
-
 //question 4
 //
-
 
 //question 5
 //
