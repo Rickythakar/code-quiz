82ef3d49d431a0919d86ae1c48a47cdd47738191
Ricky Thakar <rickythakar@gmail.com>
2022-04-10 17:23:31 -0400

    finished load question function to change question with each answer and wrote the check answer function. Also made minor changes to html to add id's and selected those id's in js. Next steps will be to adjust timer so that the answer will affect the time remaining.

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # code-quiz
 Code quiz for Coding BootCamp
 This is a code quiz for the coding bootcamp that I am working on.
+There are several comments within the function breaking down the different components of the function making the js file easy to read.
 The quiz is a timed application that will ask multiple choice questions and reward more time for answering the question correctly.
+
+
 The application will also come with a highscore table to view the top scorers.
 
 The application will be deployed at the following live link.
--- script/script.js
+++ script/script.js
@@ -1,71 +1,70 @@
-console.log ("javascript file is hooked up");
+console.log("javascript file is hooked up");
 // Declare and set variables for the sheet
 let timer = 5;
 let countdown;
 let thisQuestion = 0;
-// let quizQuestion //array
 
-// Create an array of possible choices and answers
+
+// Created an array of possible choices and answers
 let quizQuestions = [{
     question: "question 1",
-    choices: ["answer1" , "answer2" , "answer3" , "answer4"],
+    choices: ["answer1", "answer2", "answer3", "answer4"],
     answer: "answer2"
 },
 {
     question: "question 2",
-    choices: ["answer1" , "answer2" , "answer3" , "answer4"],
+    choices: ["answer1", "answer2", "answer3", "answer4"],
     answer: "answer3"
 },
-
 {
     question: "question 3",
-    choices: ["answer1" , "answer2" , "answer3" , "answer4"],
+    choices: ["answer1", "answer2", "answer3", "answer4"],
     answer: "answer4"
 },
 {
     question: "question 4",
-    choices: ["answer1" , "answer2" , "answer3" , "answer4"],
+    choices: ["answer1", "answer2", "answer3", "answer4"],
     answer: "answer2"
 },
 {
     question: "question 5",
-    choices: ["answer1" , "answer2" , "answer3" , "answer4"],
+    choices: ["answer1", "answer2", "answer3", "answer4"],
     answer: "answer4"
 }
 ];
 
+// Criteria 1 variable will be true - the false variable will remove time from the timer
 
-
-
-
-// multiple choice quiz should have 4 variables for each question
-// 1 variable will be true - the false variable will remove time from the timer
+//creating function to load question and possible choices
 function loadQuestion(questionData) {
+    var choicesElement = document.getElementById("js-choices");
     //anytime we call load question we pass through a question object from quizQuestions array
-var questionElement = document.getElementById("js-question");
-questionElement.innerText = questionData.question;
-//USING DOT NOTATION FOR QUESTION ^ QUESTION DATA IS PASSED TO THE FUNCTION AND IS PULLING DATA FROM QUESTION PROPERTY
-
-
-//BEGIN CHOICES input
-var choicesElement = document.getElementById("js-choices");
-//created this array to loop through it in the function
-var arrayOfChoices = questionData.choices;
-console.log(arrayOfChoices);
-//created a for loop to cycle through the new array for the choices 
-for (let i = 0; i < arrayOfChoices.length; i++) {
-    const choiceText = arrayOfChoices[i];
-    const choiceButton = document.createElement("button");
-    choiceButton.innerText = choiceText;
-    //calling function to append child
-    choicesElement.appendChild(choiceButton);
+    var questionElement = document.getElementById("js-question");
+    questionElement.innerText = questionData.question;
+    //USING DOT NOTATION FOR QUESTION ^ QUESTION DATA IS PASSED TO THE FUNCTION AND IS PULLING DATA FROM QUESTION PROPERTY
+    //BEGIN CHOICES input
+    choicesElement.innerHTML=""
+    //created this array to loop through it in the function
+    var arrayOfChoices = questionData.choices;
+    console.log(arrayOfChoices);
+    //created a for loop to cycle through the new array for the choices 
+    for (let i = 0; i < arrayOfChoices.length; i++) {
+        const choiceText = arrayOfChoices[i];
+        const choiceButton = document.createElement("button");
+        choiceButton.innerText = choiceText;
+        //calling function to append child
+        //APPEND CHILD APPENDS CHOICE BUTTON TO CHOICES CONTAINER ELEMENT THAT WE CREATED IN HTML
+        choiceButton.dataset.answer=choiceText;
+        choiceButton.addEventListener("click", checkAnswer);
+        choicesElement.appendChild(choiceButton);
+    }
 }
-}
+//end of loadQuestion function
 
-
-
-// Create function that connects to button to start the quiz
-function startquiz (event){ 
+// This is the function that is plugged into the startbutton that will begin the timer..
+// ..and clear the timer once the interval hits zero.
+// The end of this function wil display Game Over the html document.
+function startQuiz(event) {
     //This is the function to start the timer of the game
     countdown = setInterval(function () {
         timer--;
@@ -73,69 +72,38 @@ function startquiz (event){
         //query selector indicates its hooked up to html 
         if (timer === 0) {
             // created this if statement to stop timer at 0
-        clearInterval(countdown);
-        document.querySelector(".timer").textContent = "Game Over";
-        //if timer hits 0 text content will display game over
+            clearInterval(countdown);
+            document.querySelector(".timer").textContent = "Game Over";
+            //if timer hits 0 text content will display game over
         }
-    }, 1000);
+    }, 1000); //1000 indicates milaseconds 1000ms is equal to 1 second
     //Hides start button after starting quiz
     document.querySelector(".quiz").style.display = "none";
 
-
-    //cycle through questions on the page 
-
-    // function to show first question on the page
-loadQuestion(quizQuestions[0]);
-
-
-
-
-
-    document.querySelector(".question").textContent = quizQuestions[thisQuestion].question;
-    document.querySelector(".answers").textContent = quizQuestions[thisQuestion].answers;
-    //loop over the answers 
-
-
-    //quizQuestions[thisQuestion].answers.length
-
-    //append each one to the answers div
-
-
-
-
+    // call function to show first question on the page
+    loadQuestion(quizQuestions[thisQuestion]);
 }
 // draft of function
 // startBtn.answer function {
-//     start.timer 
-let startTimer 
+// start.timer 
+let startTimer
 
-// }
+function checkAnswer(event) {
+    const questionAnswer = quizQuestions[thisQuestion].answer;
+    const answerButtonClicked = event.target;
+    console.log(answerButtonClicked);
+    const answerButtonData = answerButtonClicked.dataset.answer;
+    if (answerButtonData === questionAnswer) 
+    {
+        thisQuestion++;
+        console.log("answer is correct")
+            // call function to show first question on the page
+    loadQuestion(quizQuestions[thisQuestion]);
+
+    }
+}
 
 // button should link to array and trigger timer
-document.querySelector(".start").addEventListener("click",startquiz)
-
-
-
-
-
-
-
-
-//question 1
-//Inside which HTML element do we put the JavaScript?
-//<javascript>
-//<js>
-//<scripting>
-//<script>
-
-//question 2
-//
-
-//queastion 3
-//
-
-//question 4
-//
-
-//question 5
-//
+document.querySelector(".start").addEventListener("click", startQuiz);
+//create a function to respond to check answer
+// document.querySelector(".button").addEventListener("click", checkAnswer);
